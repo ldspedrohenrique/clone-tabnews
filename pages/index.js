db5a391be8ec5333ea5d870cc89adc41a469ef83
Pedro Henrique <lspedrohenrique02@gmail.com>
2025-05-01 15:40:32 +0000

    adiciona conteúdo na página inicial

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ function Home(){
         <>
             <h1>Hello Next.js</h1>
             <p>Aprendendo essa bagaça</p>
+            <hr/>
+            <p>ps: Se eu te mandei o link, eu amo você s2.</p>
         </>
     )
 }
